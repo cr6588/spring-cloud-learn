716d2d7c14ecc273f6420c61cee16d962cf796f9
cr6588 <cr6588@vip.qq.com>
2018-04-21 14:49:28 +0800

    增加spring-boot-mybatis-xml-beanconfig-yml

---
+++ spring-boot-mybatis-xml-beanconfig-yml/src/main/java/com/cr6588/App.java
@@ -0,0 +1,31 @@
+package com.cr6588;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+//@EnableTransactionManagement 
+@MapperScan("com.cr6588.dao")
+//@ImportResource({"classpath:dubbo-provider.xml"})
+public class App {
+
+    //--spring.profiles.active=prod
+    public static void main(String[] args) throws Exception {
+        SpringApplication app = new SpringApplication(App.class);
+//        1在application-dev.properties中可以使用${spring.profiles.active}.会被命令行覆盖
+        Map<String, Object> defaultProperties = new HashMap<String, Object>();
+        defaultProperties.put("spring.profiles.active", "dev");
+        app.setDefaultProperties(defaultProperties);
+//        2.在application-dev.properties中无法使用${spring.profiles.active}
+//        app.setAdditionalProfiles("dev");
+        //3.--spring.profiles.active=prod
+        app.run(args);
+    }
+}
