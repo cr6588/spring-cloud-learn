a46d3c6dd43a15bd85bd807ce0ff02152d512fbc
cr6588 <cr6588@vip.qq.com>
2018-04-24 11:32:30 +0800

    spring-mybatis结构调整

--- spring-boot-mybatis/spring-boot-mybatis-xml/src/main/java/com/cr6588/App.java
+++ spring-boot-mybatis/spring-boot-mybatis-xml/src/main/java/com/cr6588/App.java
@@ -8,15 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Hello world!
  */
 @SpringBootApplication
-//@EnableTransactionManagement 
 @MapperScan("com.cr6588.dao")
-//@ImportResource({"classpath:dubbo-provider.xml"})
 public class App {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication app = new SpringApplication(App.class);
-        //启动dev配置文件
-        app.setAdditionalProfiles("dev");   // dev或prod
-        app.run(args);
+        SpringApplication.run(App.class, args);
     }
 }
