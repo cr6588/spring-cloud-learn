740bcf040d47644b74a06e539823756a4dac5a44
cr6588 <cr6588@vip.qq.com>
2018-05-14 16:44:39 +0800

    删除spring-mybatis-xml相关，增加自定义错误页面

--- spring-boot-mybatis/spring-boot-mybatis-dubbo-consumer-web/src/main/java/com/cr6588/action/ErrorController.java
+++ spring-boot-mybatis/spring-boot-mybatis-dubbo-consumer-web/src/main/java/com/cr6588/action/ErrorController.java
@@ -1,6 +1,14 @@
 package com.cr6588.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /**
@@ -8,6 +16,23 @@ import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorContro
  * @category TODO
  * @author chenyi
  */
+@Controller
 public class ErrorController extends BasicErrorController {
 
+    public ErrorController(ServerProperties serverProperties) {
+        super(new DefaultErrorAttributes(), serverProperties.getError());
+    }
+
+    @Override
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        HttpStatus status = getStatus(request);
+        String errorViewName = "500";
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                errorViewName = "404";
+            }
+        }
+        return new ModelAndView("error/" + errorViewName);
+    }
 }
