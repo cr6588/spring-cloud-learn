7e5a92c421d2123703bd982eb2e75d80bddbe76a
chenyi <chenyi@51web.com>
2019-09-09 11:30:56 +0800

    增加sprint-boot-web-init

---
+++ sprint-boot-web-init/src/main/java/com/cr6588/App.java
@@ -0,0 +1,22 @@
+package com.cr6588;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * create in 2018年04月28日
+ * @category TODO
+ * @author chenyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class App  {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication app = new SpringApplication(App.class);
+        app.run(args);
+    }
+
+}
---
+++ sprint-boot-web-init/src/main/java/com/cr6588/config/WebMvcConfig.java
@@ -0,0 +1,44 @@
+package com.cr6588.config;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * create in 2018年08月22日
+ * @category @author chenyi
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // <!-- 指定自己定义的validator -->
+    // <mvc:annotation-driven validator="validator"/>
+    // <!-- 以下 validator ConversionService 在使用 mvc:annotation-driven 会 自动注册-->
+    // <bean id="validator"
+    // class="org.springframework.validation.beanvalidation.LocalValidatorFactoryBean">
+    // <property name="providerClass"
+    // value="org.hibernate.validator.HibernateValidator"/>
+    // </bean>
+    @Override
+    public Validator getValidator() {
+        return validator();
+    }
+
+    @Bean
+    public Validator validator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setProviderClass(HibernateValidator.class);
+        return validator;
+    }
+
+    // 不需要后台处理跨域时删除
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowCredentials(true);
+    }
+
+}
